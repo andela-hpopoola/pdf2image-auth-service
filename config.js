537c4d4ad928089d5d29ceab737a5884e523c9fa
Haruna Popoola <haruna.popoola@andela.com>
2019-06-27 14:09:42 +0100

    add config files

---
+++ config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL_DEVELOPMENT,
+    dialect: 'postgres'
+  },
+  test: {
+    url: process.env.DATABASE_URL_TEST,
+    dialect: 'postgres'
+  },
+  production: {
+    url: process.env.DATABASE_URL_PRODUCTION,
+    dialect: 'postgres'
+  }
+};
