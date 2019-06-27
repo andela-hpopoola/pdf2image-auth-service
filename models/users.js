2ee74c7292ca603ec1294f2a111d000a3439176d
Haruna Popoola <haruna.popoola@andela.com>
2019-06-27 22:14:14 +0100

    Add user models

---
+++ models/users.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcryptjs');
+
+const salt = bcrypt.genSaltSync();
+
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define('Users', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'The password field cannot be empty'
+        }
+      },
+      set(password) {
+        const encryptedPassword = bcrypt.hashSync(password, salt);
+        this.setDataValue('password', encryptedPassword);
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: {
+        msg: 'Email exists in db'
+      },
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'The email field cannot be empty'
+        },
+        isEmail: {
+          msg: 'Invalid email address'
+        }
+      }
+    }
+  });
+  return Users;
+};
